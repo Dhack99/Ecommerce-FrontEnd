c1cfe7b93b92788651304ac35a85cc6c4fe17fc1
Dhanuka Iroshan <dhanukairoshan99@gmail.com>
2024-08-20 10:17:45 +0530

    create customer, admin modules and create login signup component

--- ECommerceWeb/src/app/app.module.ts
+++ ECommerceWeb/src/app/app.module.ts
@@ -3,17 +3,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DemoAngularMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
